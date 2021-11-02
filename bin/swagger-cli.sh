1f40f977c9aff6748d77aff1097db2738634e560
František Šitner <frantisek.sitner@gmail.com>
2021-11-02 16:17:33 +0100

    Fixed script for building openapi.yaml on macOS

--- bin/swagger-cli.sh
+++ bin/swagger-cli.sh
@@ -1,1 +1,2 @@
-docker run --rm -v $(dirname $(dirname $(readlink -f "$0"))):/data jeanberu/swagger-cli swagger-cli bundle /data/api.yaml --outfile /data/_build/openapi.yaml --type yaml
+PROJECT_DIRECTORY=$(dirname $(cd `dirname $0` && pwd))
+docker run --rm -v $PROJECT_DIRECTORY:/data jeanberu/swagger-cli swagger-cli bundle /data/api.yaml --outfile /data/_build/openapi.yaml --type yaml
